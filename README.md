d544da26cbe48c427f4afa1b0d66a3cf9b92f029
Kev Simpson <hello@kevsimpson.me>
2018-11-29 20:48:18 +0000

    Add readme and accordion to npm dist directory

--- README.md
+++ README.md
@@ -1,3 +1,109 @@
-a11y-accordion
+# @kevsimpson/accordion
 
-Inspired by https://frend.co
+[![npm (scoped)](https://img.shields.io/npm/v/@kevsimpson/accordion.svg)](https://www.npmjs.com/package/@kevsimpson/accordion)
+
+> Modern accessible accordion
+
+## Install
+
+```
+$ npm install @kevsimpson/accordion
+```
+
+Accordions rely on header and panel pairs, wrapped in a single container.
+
+```html
+<section class="c-accordion js-accordion">
+    <h2 id="accordion-header-1" class="c-accordion__header js-accordion__header">Accordion header 1</h2>
+    <div id="accordion-panel-1" class="c-accordion__panel js-accordion__panel">
+        ...
+    </div>
+    <h2 id="accordion-header-2" class="c-accordion__header js-accordion__header">Accordion header 2</h2>
+    <div id="accordion-panel-2" class="c-accordion__panel js-accordion__panel">
+        ...
+    </div>
+    <h2 id="accordion-header-3" class="c-accordion__header js-accordion__header">Accordion header 3</h2>
+    <div id="accordion-panel-3" class="c-accordion__panel js-accordion__panel">
+        ...
+    </div>
+</section>
+
+```
+
+An accordion effect can be achieved with minimal `SCSS` styles.
+
+```scss
+.c-accordion__panel {
+    &[aria-hidden="true"] {
+        height: 0;
+        opacity: 0;
+        pointer-events: none;
+    }
+
+    &[aria-hidden="false"] {
+        opacity: 1;
+    }
+}
+
+.c-accordion--is-ready {
+    .c-accordion__header {
+        cursor: pointer;
+    }
+
+    .c-accordion__panel {
+        overflow: hidden;
+        will-change: height, opacity;
+        transition:
+            height 0.25s ease-out,
+            opacity 0.25s ease-out;
+    }
+}
+```
+
+## Usage
+
+```js
+const myAccordion = new Accordion(document.querySelector('.js-accordion'));
+```
+
+## Methods
+
+```js
+// remove all bindings and attributes when no longer required
+myAccordion.destroy();
+
+// re-initialise if necessary
+myAccordion.init();
+```
+
+## Options
+
+```js
+const accordionContainer = document.querySelector('.js-accordion');
+const myAccordion = Fraccordion(accordionContainer, {
+	// String - Accordion header elements converted to focusable, togglable elements
+	headerSelector: '.js-accordion__header',
+
+	// String - Use header id on element to tie each accordion panel to its header - see panelIdPrefix
+	headerIdPrefix: 'accordion-header',
+
+	// String - Accordion panel elements to expand/collapse
+	panelSelector: '.js-accordion__panel',
+
+	// String - Use panel id on element to tie each accordion header to its panel - see headerIdPrefix
+	panelIdPrefix: 'accordion-panel',
+
+	// Boolean - If set to false, all accordion panels will be closed on init()
+	isFirstPanelOpen: true,
+
+	// Boolean - If set to false, each accordion instance will only allow a single panel to be open at a time
+	isMultiSelectable: true,
+
+	// String - Class name that will be added to the selector when the component has been initialised
+	readyClass: 'c-accordion--is-ready',
+});
+```
+
+---
+
+> Inspired by [Frend.co](https://frend.co)
---
+++ _dist/index.js
@@ -0,0 +1,249 @@
+/* eslint-disable no-param-reassign */
+
+class Accordion {
+    constructor(accordionContainer, {
+        headerSelector = '.js-accordion__header',
+        headerIdPrefix = 'accordion-header',
+        panelSelector = '.js-accordion__panel',
+        panelIdPrefix = 'accordion-panel',
+        isFirstPanelOpen = true,
+        isMultiSelectable = true,
+        readyClass = 'c-accordion--is-ready',
+    } = {}) {
+        if (!accordionContainer) {
+            console.error('Accordion container node is required'); // eslint-disable-line
+            return;
+        }
+
+        this.accordionContainer = accordionContainer;
+        this.headers = [...accordionContainer.querySelectorAll(headerSelector)];
+        this.panels = [...accordionContainer.querySelectorAll(panelSelector)];
+
+        this.headerIdPrefix = headerIdPrefix;
+        this.panelIdPrefix = panelIdPrefix;
+        this.isFirstPanelOpen = isFirstPanelOpen;
+        this.isMultiSelectable = isMultiSelectable;
+        this.readyClass = readyClass;
+
+        this.bindMethods();
+    }
+
+    static getPanelHeight(panel) {
+        panel.style.height = 'auto';
+        const panelHeight = panel.offsetHeight;
+        panel.style.height = '';
+        return panelHeight;
+    }
+
+    static setPanelHeight(panel) {
+        panel.addEventListener('transitionend', () => {
+            panel.style.transition = 'none';
+            panel.style.height = '';
+            panel.getBoundingClientRect();
+            panel.style.transition = '';
+        }, {
+            once: true,
+            passive: true,
+            capture: true,
+        });
+
+        const panelHeight = this.getPanelHeight(panel);
+        panel.getBoundingClientRect();
+        panel.style.height = `${panelHeight}px`;
+    }
+
+    static unsetPanelHeight(panel) {
+        const panelHeight = this.getPanelHeight(panel);
+        panel.style.height = `${panelHeight}px`;
+        panel.getBoundingClientRect();
+        panel.style.height = 0;
+    }
+
+    setHeaderFocus(index) {
+        // remove focusability from inactive headers
+        this.headers.forEach((header) => {
+            header.setAttribute('tabindex', -1);
+        });
+        // set active header focus
+        this.headers[index].setAttribute('tabindex', 0);
+        this.headers[index].focus();
+    }
+
+
+    eventHeaderClick(event) {
+        this.togglePanel(event.target);
+    }
+
+    eventHeaderKeyUp(event) {
+        const currentHeader = event.target;
+        const isModifierKey = event.metaKey || event.altKey;
+        const currentHeaderIndex = [].indexOf.call(this.headers, currentHeader);
+
+        // don't catch key events when ⌘ or Alt modifier is present
+        if (isModifierKey) return;
+
+        // catch enter/space, left/right and up/down arrow key events
+        // if new panel show it, if next/prev move focus
+        switch (event.keyCode) {
+        case 13:
+        case 32:
+            this.togglePanel(currentHeader);
+            event.preventDefault();
+            break;
+        case 37:
+        case 38: {
+            const previousHeaderIndex = (currentHeaderIndex === 0)
+                ? this.headers.length - 1
+                : currentHeaderIndex - 1;
+            this.setHeaderFocus(previousHeaderIndex);
+            event.preventDefault();
+            break;
+        }
+        case 39:
+        case 40: {
+            const nextHeaderIndex = (currentHeaderIndex < this.headers.length - 1)
+                ? currentHeaderIndex + 1
+                : 0;
+            this.setHeaderFocus(nextHeaderIndex);
+            event.preventDefault();
+            break;
+        }
+        default:
+            break;
+        }
+    }
+
+    addEventListeners() {
+        this.headers.forEach((accordionHeader) => {
+            accordionHeader.addEventListener('click', this.eventHeaderClick);
+            accordionHeader.addEventListener('keyup', this.eventHeaderKeyUp);
+        });
+    }
+
+    removeEventListeners() {
+        this.headers.forEach((accordionHeader) => {
+            accordionHeader.removeEventListener('click', this.eventHeaderClick);
+            accordionHeader.removeEventListener('keyup', this.eventHeaderKeyUp);
+        });
+    }
+
+    bindMethods() {
+        this.applyAlly = this.applyAlly.bind(this);
+        this.removeAlly = this.removeAlly.bind(this);
+        this.togglePanel = this.togglePanel.bind(this);
+        this.eventHeaderClick = this.eventHeaderClick.bind(this);
+        this.eventHeaderKeyUp = this.eventHeaderKeyUp.bind(this);
+        this.setHeaderFocus = this.setHeaderFocus.bind(this);
+    }
+
+    applyAlly() {
+        this.accordionContainer.setAttribute('role', 'tablist');
+        this.accordionContainer.setAttribute('aria-multiselectable', this.isMultiSelectable);
+
+        this.headers.forEach((header) => {
+            header.setAttribute('role', 'tab');
+            header.setAttribute('aria-controls', header.id.replace(this.headerIdPrefix, this.panelIdPrefix));
+            header.setAttribute('tabindex', 0);
+        });
+
+        this.panels.forEach((panel) => {
+            panel.setAttribute('role', 'tabpanel');
+            panel.setAttribute('aria-labelledby', panel.id.replace(this.panelIdPrefix, this.headerIdPrefix));
+            panel.setAttribute('tabindex', 0);
+        });
+    }
+
+    removeAlly() {
+        this.accordionContainer.removeAttribute('role');
+        this.accordionContainer.removeAttribute('aria-multiselectable');
+
+        this.headers.forEach((header) => {
+            header.removeAttribute('role');
+            header.removeAttribute('aria-controls');
+            header.removeAttribute('aria-selected');
+            header.removeAttribute('aria-expanded');
+            header.removeAttribute('tabindex');
+        });
+
+        this.panels.forEach((panel) => {
+            panel.removeAttribute('role');
+            panel.removeAttribute('aria-labelledby');
+            panel.removeAttribute('aria-hidden');
+            panel.removeAttribute('tabindex');
+        });
+    }
+
+    getActivePanel(targetPanelId) {
+        return this.panels.find(panel => panel.id === targetPanelId);
+    }
+
+    hidePanel(target) {
+        const targetPanelId = target.getAttribute('aria-controls');
+        const activePanel = this.getActivePanel(targetPanelId);
+        target.setAttribute('aria-selected', 'false');
+        target.setAttribute('aria-expanded', 'false');
+        this.constructor.unsetPanelHeight(activePanel);
+        activePanel.setAttribute('aria-hidden', 'true');
+    }
+
+    showPanel(target) {
+        const targetPanelId = target.getAttribute('aria-controls');
+        const activePanel = this.getActivePanel(targetPanelId);
+        target.setAttribute('tabindex', 0);
+        target.setAttribute('aria-selected', 'true');
+        target.setAttribute('aria-expanded', 'true');
+        this.constructor.setPanelHeight(activePanel);
+        activePanel.setAttribute('aria-hidden', 'false');
+    }
+
+    togglePanel(target) {
+        if (target.getAttribute('aria-selected') === 'true') {
+            this.hidePanel(target);
+            return;
+        }
+
+        if (!this.isMultiSelectable) {
+            this.hideAllPanels();
+        }
+        this.showPanel(target);
+    }
+
+    hideAllPanels() {
+        this.headers.forEach((header) => {
+            header.setAttribute('tabindex', -1);
+            header.setAttribute('aria-selected', 'false');
+            header.setAttribute('aria-expanded', 'false');
+        });
+        this.panels.forEach((panel) => {
+            if (panel.getAttribute('aria-hidden') === 'false') this.constructor.unsetPanelHeight(panel);
+            panel.setAttribute('aria-hidden', 'true');
+        });
+    }
+
+    destroy() {
+        this.removeAlly();
+        this.removeEventListeners();
+        this.accordionContainer.classList.remove(this.readyClass);
+    }
+
+    init() {
+        if (
+            !this.accordionContainer
+            || this.accordionContainer.classList.contains(this.readyClass)
+        ) return;
+        const firstHeader = this.headers[0];
+        this.applyAlly();
+        this.addEventListeners();
+        this.hideAllPanels();
+        if (this.isFirstPanelOpen) {
+            this.togglePanel(firstHeader);
+        } else {
+            firstHeader.setAttribute('tabindex', 0);
+        }
+        this.accordionContainer.classList.add(this.readyClass);
+    }
+}
+
+export default Accordion;
+
+/* eslint-enable no-param-reassign */
